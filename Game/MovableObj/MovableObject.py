d034303c1486a5b5f58eeaec7131478c1eb700d2
Nikita <avdeevn1142@gmail.com>
2024-02-05 12:36:36 +0200

    Add Ghost to game

--- Game/MovableObj/MovableObject.py
+++ Game/MovableObj/MovableObject.py
@@ -1,6 +1,12 @@
+from Game.Game_Controllers.Direction import Direction
 from Game.Game_Controllers.Game import GameObject
 
 
 class MovableObject(GameObject):
-    def __init__(self):
-        self
+    def __init__(self, in_surface, x, y, in_size: int, in_color=(255, 0, 0), is_circle: bool = False):
+        super().__init__(in_surface, x, y, in_size, in_color, is_circle)
+        self.current_direction = Direction.NONE
+        self.direction_buffer = Direction.NONE
+        self.last_working_direction = Direction.NONE
+        self.location_queue = []
+        self.next_target = None
