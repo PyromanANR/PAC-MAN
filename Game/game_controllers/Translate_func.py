1f30597ded97c21a8356ccc9b3ef9e9bddd0af9e
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2024-02-19 14:06:41 +0200

    change map size

--- Game/game_controllers/Translate_func.py
+++ Game/game_controllers/Translate_func.py
@@ -1,4 +1,4 @@
-unified_size = 40
+unified_size = 35
 # General functions for coordinate conversion
 def translate_screen_to_maze(in_coords, in_size = unified_size):
     return int(in_coords[0] / in_size), int(in_coords[1] / in_size)
